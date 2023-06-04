288798246f7aabcf446675420ee889ad2c4764e2
DonnywoodMac <blaskwood@gmail.com>
2023-06-04 15:34:55 +0200

    Change Mac

--- 24c.js
+++ 24c.js
@@ -11,33 +11,32 @@ const personalMovieDB = {
 };
 
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = prompt('На сколько оцениваете его?', '');
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('Один из последних просмотренных фильмов?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');  
-    } else {
-        console.log('error');
-        i--;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');  
+        } else {
+            console.log('error');
+            i--;
+        }
     }
-    
-    
-    
-    personalMovieDB.movies[a] = b;
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Мало фильмов смотрешь,дурачёк");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Классический проёбщик времени");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Looool zadrot");
-} else {
-    console.log("ERROR MAZAFAK");
-}
+rememberMyFilms();
+
+    if (personalMovieDB.count < 10) {
+        console.log("Мало фильмов смотрешь,дурачёк");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Классический проёбщик времени");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Looool zadrot");
+    } else {
+        console.log("ERROR MAZAFAK");
+    }
 
 console.log(personalMovieDB);
 
