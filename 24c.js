c227a6ce494aa5ac36b33ba8e69240951f426418
Donnywood <Blaskwood@gmail.com>
2023-06-20 16:09:51 +0200

    1

--- 24c.js
+++ 24c.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let number0Films;
+
+function start() {
+    number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (number0Films == '' || number0Films == null || isNaN(number0Films)) {
+        number0Films = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+
+}
+
+start();
 
 const personalMovieDB = {
     count: number0Films,
@@ -12,35 +23,55 @@ const personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('Один из последних просмотренных фильмов?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');  
-    } else {
-        console.log('error');
-        i--;
+
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('Один из последних просмотренных фильмов?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');  
+        } else {
+            console.log('error');
+            i--;
+        }
     }
-    
-    
-    
-    personalMovieDB.movies[a] = b;
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Мало фильмов смотрешь,дурачёк");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Классический проёбщик времени");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Looool zadrot");
-} else {
-    console.log("ERROR MAZAFAK");
+// rememberMyFilms();
+// 
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Мало фильмов смотрешь,дурачёк");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Классический проёбщик времени");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Looool zadrot");
+    } else {
+        console.log("ERROR MAZAFAK");
+    }
 }
 
-console.log(personalMovieDB);
+// detectPersonalLevel();
 
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.generes[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
 
 
     
